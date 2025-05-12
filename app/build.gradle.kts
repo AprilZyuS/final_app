291ad7759157091906f41c0d20f3c3f911411b4b
Yuze Sun <497466295@qq.com>
2025-05-12 10:46:50 +0800

    依赖搭建完成

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation()
+    implementation(libs.mlkit.pose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
