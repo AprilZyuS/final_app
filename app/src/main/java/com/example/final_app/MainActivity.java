013f4887f2bc6235df70cad941c510e3fbed81b7
Yuze Sun <497466295@qq.com>
2025-05-12 11:21:32 +0800

    建了几个小类~

--- app/src/main/java/com/example/final_app/MainActivity.java
+++ app/src/main/java/com/example/final_app/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.final_app;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.PoseDetector;
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        PoseDetectorOptions options =
+                new PoseDetectorOptions.Builder()
+                        .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+                        .build();
+        PoseDetector poseDetector = PoseDetection.getClient(options);
+    }
+
+    private class YourAnalyzer implements ImageAnalysis.Analyzer {
+
+        @androidx.camera.core.ExperimentalGetImage
+        @Override
+        public void analyze(ImageProxy imageProxy) {
+            Image mediaImage = imageProxy.getImage();
+            if (mediaImage != null) {
+                InputImage image =
+                        InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                // Pass image to an ML Kit Vision API
+                // ...
+            }
+        }
     }
 }
